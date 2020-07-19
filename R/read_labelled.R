9de3e21d3812dcd0e7e3873dfca16699786664a1
Alexander Vinogradov <a.g.vinogradov@gmail.com>
2020-07-19 11:57:55 +0300

    elipsis args

--- R/read_labelled.R
+++ R/read_labelled.R
@@ -9,9 +9,9 @@
 #' @importFrom jsonlite fromJSON
 #' @importFrom haven labelled
 #' @export
-read_labelled <- function(file, data.sheet = 1, vars.sheet = 2) {
-  data <- read_excel(file, sheet = data.sheet)
-  vars <- read_excel(file, sheet = vars.sheet)
+read_labelled <- function(file, data.sheet = 1, vars.sheet = 2, ...) {
+  data <- read_excel(file, sheet = data.sheet, ...)
+  vars <- read_excel(file, sheet = vars.sheet, ...)
 
   if (!"variable" %in% names(vars)) stop("There is no 'variable' column in dictionary", call. = FALSE)
   if (!"label" %in% names(vars)) stop("There is no 'label' column in dictionary", call. = FALSE)
